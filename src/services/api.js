c54be3df620f5be50648999c35e152af000b4291
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-04-13 17:50:22 +0200

    ok

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,12 @@
 import axios from 'axios'
 
+// export const instance = axios.create({
+//   baseURL: 'https://dummyjson.com/'
+// })
+
+API_KEY = 'f9f6d3f14431911aa9602e018f8e4b77'
+
 export const instance = axios.create({
-  baseURL: 'https://dummyjson.com/'
+  baseURL:
+    'https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false'
 })
